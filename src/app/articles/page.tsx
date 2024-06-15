211529927364526e0440adeff6478a6eb9621dfc
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-15 19:46:13 +0200

    feat: update articles & create first "mon parcours de développeur"

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -21,7 +21,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
           {formatDate(article.date)}
         </Card.Eyebrow>
         <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
+        <Card.Cta>Lire l'article</Card.Cta>
       </Card>
       <Card.Eyebrow
         as="time"
@@ -37,7 +37,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
 export const metadata: Metadata = {
   title: 'Articles',
   description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+    'Mes pensées sur la programmation, la société numérique et mes side projects.',
 }
 
 export default async function ArticlesIndex() {
@@ -45,8 +45,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="Mes pensées sur la programmation, la société numérique et mes side projects."
+      intro="Mes idées rédigées humblement, autour des sujets qui me passionnent."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
