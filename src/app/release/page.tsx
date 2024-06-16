f49fa7f51de2c7c9c7bc915eafc3e4cb1a500e38
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-16 16:51:54 +0200

    feat: update release page presentation

--- src/app/release/page.tsx
+++ src/app/release/page.tsx
@@ -69,35 +69,29 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            Je m'appelle Dimitri Bourreau, bourreau des codes, ceux qu'on améliore !
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              Aussi loin que je me souvienne j'ai toujours aimé les nouvelles technologies.
+              J'ai cassé mon premier Windows 98 à 10 ans, et j'ai fait mes premiers sites moches au CDI du collège, avec
+              l'aide du site du zéro.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              J'ai fait un détour par les avions, en me formant au métier de pilote de ligne.
+              Mais avant de pouvoir en faire mon métier, je suis devenu inapte au vol.
+              Des rêves réalisés et des nuages caressés, j'ai atterri vers ma première passion : le code.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              J'ai suivi une formation d'un an avec Openclassrooms, puis j'ai passé quelques mois en auto-formation à temps plein.
+              5 ans plus tard, j'ai aidé une dizaine d'entreprises à améliorer leurs applications, je cumule sur Malt 40 missions
+              avec une note de 5/5 pour 31 avis.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              J'ai créé ma société RELEASE en référence au groupe de musique Starrysky et leur musique RE:LEASE.
+              Cette musique dure 20 minutes, elle frappe par sa qualité et son originalité.
+              Ce groupe Starrysky existe depuis des années, ils ont une passion brûlante pour le bien-faire et l'originalité.
+              Je veux inspirer la même passion et ténacité !
             </p>
           </div>
         </div>
