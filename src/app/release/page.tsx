8fde6b6382e0d505a372b003b91c748bdc681e1b
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-16 18:34:14 +0200

    refactor: remove unused imports & files

--- src/app/release/page.tsx
+++ src/app/release/page.tsx
@@ -6,9 +6,7 @@ import clsx from 'clsx'
 import { Container } from '@/components/Container'
 import {
   GitHubIcon,
-  InstagramIcon,
   LinkedInIcon,
-  XIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
 
