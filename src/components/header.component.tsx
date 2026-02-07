8b90d1df598112c4c32d6d45495a2ac8130550de
Dimitri Bourreau <dimitri@release-dev.com>
2026-02-07 13:16:17 +0100

    feat: remove testimonials

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -6,7 +6,6 @@ const navLinks = [
   { label: 'Dimitri', href: '/dimitri' },
   { label: 'Expérience', href: '/experience' },
   { label: 'Projets', href: '/projets' },
-  { label: 'Témoignages', href: '/temoignages' },
   { label: 'Blog', href: '/blog' },
   { label: 'Ressources', href: '/ressources' },
   { label: 'Contact', href: '/contact' },
